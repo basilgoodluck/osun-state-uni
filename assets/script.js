1d5efbeaad5760a683e8235e95f7a7133f0c6f8e
Basil Goodluck <basilgoodluck22@gmail.com>
2023-11-24 18:35:31 +0100

    almost done

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+const grays = document.querySelectorAll(".gray")
+
+grays.forEach((gray)=>{
+    gray.addEventListener("mouseover", ()=>{
+        gray.style.setProperty("--changeDir", "65%");
+        gray.style.setProperty("--opaq", "1");
+    });
+
+    gray.addEventListener("mouseout", ()=>{
+        gray.style.setProperty("--changeDir", "-50%");
+        gray.style.setProperty("--opaq", ".3");
+    });
+})
+
+const navMenu = document.querySelector(".home .header .navMenu")
+
+const show = function(){
+    navMenu.classList.toggle("showmenu")
+
+}
